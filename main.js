25962d6ffccc97b91c6eeb6638de7a3bd0e025a6
KrisCastillo <criscastillo2@hotmail.com>
2020-02-11 00:07:53 -0500

    added all files

---
+++ main.js
@@ -0,0 +1,24 @@
+	$('#crisProject').hide();
+	$('#crisInfo').hide(); 
+	$('#crisBio').hide();
+	$('body').on('click','#myBtn', (event) => {
+		myPortfolio();
+	})
+
+function myPortfolio() {
+	$("#crisIntro").hide();
+	$("#crisProject").hide();
+	$("#crisBio").show();
+	$("#crisInfo").show();
+	$('body').on('click','#projects', (event) => {
+		myWork();
+	})
+}
+
+function myWork() {
+	$("#crisIntro").hide();
+	$("#crisBio").hide();
+	$("#crisProject").show();	
+	$("#crisInfo").show();
+}
+
